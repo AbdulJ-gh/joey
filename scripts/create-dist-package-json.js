6336c57e7a8ec466c89804a260adb2bb0b177f03
Abdul Jabbar <abduljabbar.git@gmail.com>
2021-10-03 06:30:09 +0100

    added package json dist script

---
+++ scripts/create-dist-package-json.js
@@ -0,0 +1,9 @@
+const { readFileSync, writeFileSync } = require('fs');
+
+const packageJson = JSON.parse(readFileSync('./package.json', { encoding: 'utf8' }));
+
+delete packageJson.scripts;
+delete packageJson.dependencies;
+delete packageJson.devDependencies;
+
+writeFileSync('./lib/package.json', JSON.stringify(packageJson, null, 2));
