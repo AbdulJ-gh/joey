bb6ae5f3c3aab5815d31c2192addd8571cccb8ff
Abdul Jabbar <abduljabbar.git@gmail.com>
2021-12-17 02:22:34 +0000

    updated configs

--- ava.config.js
+++ ava.config.js
@@ -2,9 +2,11 @@ export default {
 	require: ['esm', './testUtils/testRuntimeGlobals.js'],
 	files: ['src/**/*.test.*'],
 	typescript: {
-		rewritePaths: { 'src/': 'lib/' },
+		rewritePaths: { 'src/': 'lib-test/src/' },
 		compile: 'tsc'
 	},
+	verbose: true,
 	cache: false,
-	timeout: '10s'
+	timeout: '10s',
+	ignoredByWatcher: ['lib/', 'lib-test/']
 };
--- scripts/create-dist-package-json.js
+++ scripts/create-dist-package-json.js
@@ -4,5 +4,6 @@ const packageJson = JSON.parse(readFileSync('./package.json', { encoding: 'utf8'
 
 delete packageJson.scripts;
 delete packageJson.devDependencies;
+delete packageJson.publishConfig;
 
 writeFileSync('./lib/package.json', JSON.stringify(packageJson, null, 2));
