354933c0b67b8378d038cfbbaccb967813decc6a
Abdul Jabbar <abduljabbar.git@gmail.com>
2021-12-17 05:55:53 +0000

    removed comment

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import Routing from './Routing';
 import Transforms from './Transforms';
 import Crypto from './Crypto';
 import Utilities from './Utilities';
-/** NOTE: Current code is not tree shakeable to method level, only to module level I thinks. Fix this. **/
 
 export default Joey;
 export { Routing, Transforms, Crypto, Utilities };
