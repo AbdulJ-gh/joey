b90fcc45b7dbd9f0d2232f4ab7cf86674a4b3998
Abdul Jabbar <abduljabbar.git@gmail.com>
2021-12-18 01:29:05 +0000

    optimised exports

--- src/Transforms/index.ts
+++ src/Transforms/index.ts
@@ -2,5 +2,5 @@ import Base64 from './base64';
 import ByteArray from './byteArray';
 import HexString from './hexString';
 
-const Transforms = { Base64, ByteArray, HexString };
-export default Transforms;
+export { Base64, ByteArray, HexString };
+// export default ({ Base64, ByteArray, HexString });
