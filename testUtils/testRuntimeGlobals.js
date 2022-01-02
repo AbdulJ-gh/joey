8a01e7a95bdd4fdce006480cfe3a03dd9c1e765b
Abdul Jabbar <abduljabbar.git@gmail.com>
2022-01-02 18:15:51 +0000

    added crypto global

--- testUtils/testRuntimeGlobals.js
+++ testUtils/testRuntimeGlobals.js
@@ -1,8 +1,10 @@
 import { fetch, Request, Response, Headers } from 'cross-fetch';
 import FormData from 'form-data';
+import { webcrypto } from 'crypto';
 
 globalThis.fetch = fetch;
 globalThis.Request = Request;
 globalThis.Response = Response;
 globalThis.Headers = Headers;
 globalThis.FormData = FormData;
+globalThis.crypto = webcrypto;
