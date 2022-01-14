a7be30be1b6f92cdc49a17402a73301d4757a7cd
Abdul Jabbar <abduljabbar.git@gmail.com>
2022-01-14 23:58:23 +0000

    Updated Authenticator

--- src/Routing/Authenticator/authenticator.test.ts
+++ src/Routing/Authenticator/authenticator.test.ts
@@ -1,59 +1,59 @@
 import test from 'ava';
-import { Res } from '../Res';
+import { generateMockContext } from '../../../testUtils/generateMockContext';
 import { Authenticator } from './index';
-import type { AuthData, AuthHandler } from './types';
 
-class TestAuthenticator extends Authenticator {
-	constructor(authHandler: AuthHandler) {
-		super(authHandler);
-	}
-	public getAuthHAndler(): AuthHandler { return this.authHandler; }
-	public getAuthData(): AuthData { return this.authData; }
-}
-
-const context = { req: new Request(''), res: new Res() };
 
 test('Authenticator - Properties', t => {
 	const authHandler = () => null;
-	const authenticator = new TestAuthenticator(authHandler);
-	t.deepEqual(authenticator.getAuthHAndler(), authHandler);
-	t.deepEqual(authenticator.getAuthData(), null);
+	const authenticator = new Authenticator(authHandler);
+	t.is(authenticator.authHandler, authHandler);
 });
 
-test('Authenticator - Auth data', async t => {
-	const authenticator = new TestAuthenticator(() => ({ some: 'data' }));
-	await authenticator.authenticate(context);
-	t.deepEqual(authenticator.getAuthData(), { some: 'data' });
+test('Authenticator - Handler returns data', async t => {
+	const context = generateMockContext();
+	const authenticator = new Authenticator(() => ({ some: 'data' }));
+	t.deepEqual(authenticator.authHandler(context), { some: 'data' });
+	t.true(await authenticator.authenticate(context));
+	t.deepEqual(context.req.auth, { some: 'data' });
 });
 
-
-test('Authenticator - Authenticate - no/null authHandler', async t => {
-	const authenticator = new TestAuthenticator(() => null);
-	const result = await authenticator.authenticate(context);
-	t.true(result);
+test('Authenticator - Handler returns null', async t => {
+	const context = generateMockContext();
+	const authenticator = new Authenticator(() => null);
+	t.deepEqual(authenticator.authHandler(context), null);
+	t.true(await authenticator.authenticate(context));
+	t.is(context.req.auth, null);
 });
 
-test('Authenticator - Authenticate - successful authentication', async t => {
-	const authenticator = new TestAuthenticator(() => ({ any: 'data' }));
-	const result = await authenticator.authenticate(context);
-	t.true(result);
+test('Authenticator - Handler returns true', async t => {
+	const context = generateMockContext();
+	const authenticator = new Authenticator(() => true);
+	t.true(authenticator.authHandler(context));
+	t.true(await authenticator.authenticate(context));
+	t.deepEqual(context.req.auth, null);
 });
-//
-test('Authenticator - Authenticate - failed authentication', async t => {
-	const authenticator = new TestAuthenticator(() => false);
-	const result = await authenticator.authenticate(context);
-	t.false(result);
+
+test('Authenticator - Handler returns false', async t => {
+	const context = generateMockContext();
+	const authenticator = new Authenticator(() => false);
+	t.false(authenticator.authHandler(context));
+	t.false(await authenticator.authenticate(context));
+	t.deepEqual(context.req.auth, null);
 });
-//
-test('Authenticator - Authenticate - Return Response directly', async t => {
-	const mockResponse = new Response();
-	const authenticator = new TestAuthenticator(() => mockResponse);
-	const result = await authenticator.authenticate(context);
-	t.is(result, mockResponse);
+
+test('Authenticator - return Res directly', async t => {
+	const context = generateMockContext();
+	const authenticator = new Authenticator(({ res }) => res);
+	t.is(authenticator.authHandler(context), context.res);
+	t.is(await authenticator.authenticate(context), context.res);
+	t.deepEqual(context.req.auth, null);
 });
-//
-test('Authenticator - Authenticate - Return Res directly', async t => {
-	const authenticator = new TestAuthenticator(() => new Res());
-	const result = await authenticator.authenticate(context);
-	t.deepEqual(result, new Response(null, { status: 204 }));
+
+test('Authenticator - return Response directly', async t => {
+	const context = generateMockContext();
+	const response = new Response;
+	const authenticator = new Authenticator(() => response);
+	t.is(authenticator.authHandler(context), response);
+	t.is(await authenticator.authenticate(context), response);
+	t.deepEqual(context.req.auth, null);
 });
--- src/Routing/Authenticator/authenticator.ts
+++ src/Routing/Authenticator/authenticator.ts
@@ -1,22 +1,22 @@
 import { Res } from '../Res';
-import { Responder } from '../Responder';
 import type { Context } from '../Router';
-import type { AuthData, AuthHandler } from './types';
+import type { AuthHandler } from './types';
 
 export class Authenticator {
 	public authHandler: AuthHandler;
-	public authData: AuthData = null;
 
 	constructor(authHandler: AuthHandler) {
 		this.authHandler = authHandler;
 	}
 
-	public async authenticate(context: Context): Promise<Response | boolean> {
+	public async authenticate(context: Context): Promise<Res | Response | boolean> {
 		const authResponse = await this.authHandler(context);
-		if (authResponse instanceof Res) return new Responder(authResponse).respond();
-		if (authResponse instanceof Response) return authResponse;
-		this.authData = authResponse;
-		if (this.authData) context.req.authData = this.authData;
-		return this.authData !== false; // If authData === null, this returns true and allows the request to proceed unauthenticated
+
+		if (authResponse instanceof Res || authResponse instanceof Response || typeof authResponse === 'boolean') {
+			return authResponse;
+		}
+
+		context.req.auth = authResponse;
+		return true;
 	}
 }
