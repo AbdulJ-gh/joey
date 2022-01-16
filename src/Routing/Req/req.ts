fc7db40dea77d902ac1ca7f27989622f98c8e41a
Abdul Jabbar <abduljabbar.git@gmail.com>
2022-01-16 01:54:55 +0000

    renamed parameterizedPath to absolutePath

--- src/Routing/Req/req.ts
+++ src/Routing/Req/req.ts
@@ -10,17 +10,17 @@ export class Req extends Request {
 		super(request);
 	}
 
-	private static pathHasParams(parameterizedPath: string): boolean {
-		return parameterizedPath.includes('/:');
+	private static pathHasParams(absolutePath: string): boolean {
+		return absolutePath.includes('/:');
 	}
 
-	public static getPathParams(req: Req, parameterizedPath: string): void {
+	public static getPathParams(req: Req, absolutePath: string): void {
 		const path = new URL(req.url).pathname;
 
-		if (this.pathHasParams(parameterizedPath)) {
+		if (this.pathHasParams(absolutePath)) {
 			const params: Record<string, string> = {};
 			const pathSplit = path.split('/');
-			const paramPathSplit = parameterizedPath.split('/');
+			const paramPathSplit = absolutePath.split('/');
 
 			for (let i = 0; i < pathSplit.length; i++) {
 				if (paramPathSplit[i].startsWith(':')) {
