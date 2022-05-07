464d059b4ccd5b5ab713e3720676f83d548dc3d6
Abdul Jabbar <abduljabbar.git@gmail.com>
2022-05-07 17:27:14 +0100

    Moved res types out and added generic defaults

--- src/Core/types.ts
+++ src/Core/types.ts
@@ -1,6 +1,6 @@
 import type Context from './context';
 import type { Config } from './config';
-import { Res } from './res';
+import { Res, ResponseBody } from './res';
 
 
 export type DeserialisedJson = Record<string, unknown> | unknown[];
@@ -8,19 +8,6 @@ export type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'OPTIONS'; //
 
 
 /** Body */
-export type ResponseBody = null | string | DeserialisedJson | ArrayBuffer | Blob | URLSearchParams | FormData;
-export type ResponseType =
-	| 'noContent'
-	| 'plaintext'
-	| 'json'
-	| 'arrayBuffer'
-	| 'typedArray'
-	| 'blob'
-	| 'urlEncodedFormData'
-	| 'formData'
-
-// This allows us to send back FormData or x-www-form-urlencoded
-// @cf type BodyInit = ReadableStream | string | ArrayBuffer | Blob | URLSearchParams | FormData;
 
 /** Response */
 export type ResponseObject = {
@@ -32,15 +19,19 @@ export type ResponseObject = {
 export type ResponseLike = Response | ResponseObject | Res;
 
 /** Handlers */
-type SyncMiddlewareHandler<ENV, DEPS, REQ> = (ctx: Context<ENV, DEPS, REQ>) => ResponseLike | void;
-export type AsyncMiddlewareHandler<ENV, DEPS, REQ> = (ctx: Context<ENV, DEPS, REQ>) => Promise<ResponseLike | void>;
+type SyncMiddlewareHandler<ENV = unknown, DEPS = unknown, REQ = unknown> =
+	(ctx: Context<ENV, DEPS, REQ>) => ResponseLike | void;
+export type AsyncMiddlewareHandler<ENV = unknown, DEPS = unknown, REQ = unknown> =
+	(ctx: Context<ENV, DEPS, REQ>) => Promise<ResponseLike | void>;
 
 export type MiddlewareHandler<ENV = unknown, DEPS = unknown, REQ = unknown> =
 	| SyncMiddlewareHandler<ENV, DEPS, REQ>
 	| AsyncMiddlewareHandler<ENV, DEPS, REQ>;
 
-export type SyncHandler<ENV, DEPS, REQ> = (ctx: Context<ENV, DEPS, REQ>) => ResponseLike;
-export type AsyncHandler<ENV, DEPS, REQ> = (ctx: Context<ENV, DEPS, REQ>) => Promise<ResponseLike>;
+export type SyncHandler<ENV = unknown, DEPS = unknown, REQ = unknown> =
+	(ctx: Context<ENV, DEPS, REQ>) => ResponseLike;
+export type AsyncHandler<ENV = unknown, DEPS = unknown, REQ = unknown> =
+	(ctx: Context<ENV, DEPS, REQ>) => Promise<ResponseLike>;
 
 export type Handler<ENV = unknown, DEPS = unknown, REQ = unknown> =
 	SyncHandler<ENV, DEPS, REQ> | AsyncHandler<ENV, DEPS, REQ>;
