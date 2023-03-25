4c0e3085436c367a36218b3e96b972b831492ab7
Abdul Jabbar <abduljabbar.git@gmail.com>
2023-03-25 02:54:32 +0000

    Added formdata util

---
+++ src/Utilities/formData/formData.ts
@@ -0,0 +1,18 @@
+/*
+	* Limitations
+	* Accepts both `application/x-www-form-urlencoded` and `multipart/form-data` content
+	* Only works with string form values, no file uploads
+	* */
+
+type FormInput = string | File;
+
+export function getFormData(formData: FormData) {
+	const data: Record<string, FormInput|FormInput[]> = {};
+	for (const [key, value] of formData) {
+		if (!data[key]) {
+			const val = formData.getAll(key);
+			data[key] = val.length === 1 ? val[0] : val;
+		}
+	}
+	return data;
+}
